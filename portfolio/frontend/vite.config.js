ae5ef3940cd5571a8e82ff5bf08ec9b048217cfb
getaye21 <getayefiseha21@gmail.com>
2026-04-03 19:03:38 +0300

    Update vite.config.js

--- portfolio/frontend/vite.config.js
+++ portfolio/frontend/vite.config.js
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      }
-    }
-  },
   build: {
     outDir: 'dist',
     emptyOutDir: true
